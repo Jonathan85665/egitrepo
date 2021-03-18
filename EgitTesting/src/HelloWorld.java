b1da15c8568ae8334b4f9ea1eb4c3a4689a29072
jonathan <jonathan@macbook-air.attlocal.net>
2021-03-17 19:50:45 -0500

    new-feature added

--- EgitTesting/src/HelloWorld.java
+++ EgitTesting/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("What is the cost of living?");
-
+		System.out.println("This is the new feature its pretty cool");
 	}
 
 }
